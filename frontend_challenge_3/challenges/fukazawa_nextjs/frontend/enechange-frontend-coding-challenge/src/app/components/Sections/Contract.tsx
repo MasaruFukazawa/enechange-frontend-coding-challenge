e10e2dceca68d1e21a3e3a67345e9f8472ef8a14
Masaru Fukazawa <upw.ponsuke@gmail.com>
2025-02-02 20:30:19 +0900

    使用状況セクションを作成

---
+++ frontend_challenge_3/challenges/fukazawa_nextjs/frontend/enechange-frontend-coding-challenge/src/app/components/Sections/Contract.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import SectionTitle from '../atoms/SectionTitle';
+import CompanyModule from '../molecules/Company';
+import PlanModule from '../molecules/Plan';
+import ContractedCapacityModule from '../molecules/ContractedCapacity';
+
+interface ZipcodeProps {
+  sectionTitleText: string;
+  companyInputTitleText: string;
+  companyInputTitleTextIsRequired: boolean;
+  companySelectBoxName: string;
+  companySelectBoxOptions: { name: string }[];
+  companyErrorMessageText: string;
+  planInputTitleText: string;
+  planInputTitleTextIsRequired: boolean;
+  planSelectBoxName: string;
+  planSelectBoxOptions: { name: string }[];
+  planErrorMessageText: string;
+  contractedCapacityInputTitleText: string;
+  contractedCapacityInputTitleTextIsRequired: boolean;
+  contractedCapacitySelectBoxName: string;
+  contractedCapacitySelectBoxOptions: { name: string }[];
+  contractedCapacityErrorMessageText: string;
+} 
+
+const Zipcode: React.FC<ZipcodeProps> = ({ 
+  sectionTitleText,
+  companyInputTitleText,
+  companyInputTitleTextIsRequired,
+  companySelectBoxName,
+  companySelectBoxOptions,
+  companyErrorMessageText,
+  planInputTitleText,
+  planInputTitleTextIsRequired,
+  planSelectBoxName,
+  planSelectBoxOptions,
+  planErrorMessageText,
+  contractedCapacityInputTitleText,
+  contractedCapacityInputTitleTextIsRequired,
+  contractedCapacitySelectBoxName,
+  contractedCapacitySelectBoxOptions,
+  contractedCapacityErrorMessageText,
+}) => {
+  return (
+    <section 
+      id='contract'
+      className='pt-4'
+    >
+      <SectionTitle
+        text={sectionTitleText} 
+      />
+      <div className='pl-2 pt-8'>
+        <CompanyModule
+          inputTitleText={companyInputTitleText}
+          inputTitleTextIsRequired={companyInputTitleTextIsRequired}
+          selectBoxName={companySelectBoxName}
+          selectBoxOptions={companySelectBoxOptions}
+          errorMessageText={companyErrorMessageText}
+        />
+      </div>
+      <div className='pl-2 pt-8'>
+        <PlanModule
+          inputTitleText={planInputTitleText}
+          inputTitleTextIsRequired={planInputTitleTextIsRequired}
+          selectBoxName={planSelectBoxName}
+          selectBoxOptions={planSelectBoxOptions}
+          errorMessageText={planErrorMessageText}
+        />
+      </div>
+      <div className='pl-2 pt-8'>
+        <ContractedCapacityModule
+          inputTitleText={contractedCapacityInputTitleText}
+          inputTitleTextIsRequired={contractedCapacityInputTitleTextIsRequired}
+          selectBoxName={contractedCapacitySelectBoxName}
+          selectBoxOptions={contractedCapacitySelectBoxOptions}
+          errorMessageText={contractedCapacityErrorMessageText}
+        />
+      </div>
+
+    </section>
+  );
+};
+
+export default Zipcode;
