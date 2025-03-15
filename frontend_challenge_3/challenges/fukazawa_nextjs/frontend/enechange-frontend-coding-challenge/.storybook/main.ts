0c763020d91b1812d294e08ce360413682211328
Masaru Fukazawa <upw.ponsuke@gmail.com>
2025-03-16 08:12:31 +0900

    環境変数:API_BASE_URLを設定

--- frontend_challenge_3/challenges/fukazawa_nextjs/frontend/enechange-frontend-coding-challenge/.storybook/main.ts
+++ frontend_challenge_3/challenges/fukazawa_nextjs/frontend/enechange-frontend-coding-challenge/.storybook/main.ts
@@ -1,4 +1,15 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import webpack from "webpack";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import dotenv from "dotenv";
+
+// __dirname を ESM で再現
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Storybook の環境変数をロード
+dotenv.config({ path: resolve(__dirname, "../.env.storybook") });
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -13,5 +24,14 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ["../public"],
+  // 環境変数を Webpack に注入
+  webpackFinal: async (config) => {
+    config.plugins?.push(
+      new webpack.DefinePlugin({ // webpack. をつける
+        "process.env.NEXT_PUBLIC_API_BASE_URL": JSON.stringify(process.env.NEXT_PUBLIC_API_BASE_URL),
+      })
+    );
+    return config;
+  },
 };
 export default config;
